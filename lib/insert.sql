e131714df0c56f6008b80267806b2df3eccb00ea
syd333 <sydmiranda@icloud.com>
2021-01-22 19:36:22 -0500

    commit tree aa52c13c7f527393c8644eb61eebab6b5328fa56

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("b", 23, 'M', 'black', 'happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("e", 10, 'M', 'red', 'mad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("q", 44, 'M', 'orange', 'sad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("o", 68, 'F', 'pink', 'mad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("z", 79, 'F', 'aqua', 'content', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("d", 31, 'F', 'brown', 'sad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("f", 102, 'M', 'purple', 'hungry', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 52, 'F', 'blue', 'mad', true);
